c36dd32841c129fc38c71488329131ca6248f3f4
Joel Potter <ithielnor@gmail.com>
2017-11-07 13:57:02 -0500

    Add microdeposit model

---
+++ Dwolla.Client/Models/Requests/MicroDepositsRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Dwolla.Client.Models.Requests
+{
+    public class MicroDepositsRequest
+    {
+        public Amount Amount1 { get; set; }
+        public Amount Amount2 { get; set; }
+    }
+
+    public class Amount
+    {
+        public decimal Value { get; set; }
+        public string Currency { get; set; }
+    }
+}
