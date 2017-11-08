67c9b34eec3aa43e6236c357cd5a559d4f4f58da
Joel Potter <ithielnor@gmail.com>
2017-11-08 13:12:49 -0500

    Event models

---
+++ Dwolla.Client/Models/Responses/EventResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dwolla.Client.Models.Responses
+{
+    public class EventResponse : BaseResponse
+    {
+        public string Id { get; set; }
+        public DateTime Created { get; set; }
+        public string Topic { get; set; }
+        public string ResourceId { get; set; }
+    }
+}
