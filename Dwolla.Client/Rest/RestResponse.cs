9729d47fe12656c8a7172644cb3b2b942dc2caf0
Joel Potter <ithielnor@gmail.com>
2017-11-14 14:14:33 -0500

    Expose raw response content on RestResponse

--- Dwolla.Client/Rest/RestResponse.cs
+++ Dwolla.Client/Rest/RestResponse.cs
@@ -6,13 +6,15 @@ namespace Dwolla.Client.Rest
     {
         public HttpResponseMessage Response { get; }
         public T Content { get; }
+        public string RawContent { get; }
         public RestException Exception { get; }
 
-        public RestResponse(HttpResponseMessage response, T content, RestException exception = null)
+        public RestResponse(HttpResponseMessage response, T content, RestException exception = null, string rawContent = null)
         {
             Response = response;
             Exception = exception;
             Content = content;
+            RawContent = rawContent;
         }
     }
 }
