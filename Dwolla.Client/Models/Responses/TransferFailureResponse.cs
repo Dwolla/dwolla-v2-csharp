a073e793f11e59464bff27e5507b77f3ff1f0c3d
Joel Potter <ithielnor@gmail.com>
2017-12-06 16:13:40 -0500

    Add transfer failure model and example

---
+++ Dwolla.Client/Models/Responses/TransferFailureResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dwolla.Client.Models.Responses
+{
+    public class TransferFailureResponse : BaseResponse
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Explanation { get; set; }
+    }
+}
---
+++ ExampleApp/Tasks/Transfers/GetFailure.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace ExampleApp.Tasks.Transfers
+{
+    [Task("gtf", "Get Transfer Failure")]
+    class GetFailre : BaseTask
+    {
+        public override async Task Run()
+        {
+            Write("Transfer ID to retrieve the failure: ");
+            var input = ReadLine();
+
+            var failure = await Broker.GetTransferFailureAsync(input);
+
+            WriteLine($"Code: {failure.Code}; Description: {failure.Description};");
+        }
+    }
+}
