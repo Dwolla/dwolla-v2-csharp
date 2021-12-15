ee7c6b05d89e48ce950417929cd592e09c6f70af
Peter <kerr_peter@hotmail.com>
2021-12-15 17:22:24 +0000

    Models for Creating a Funding Source

---
+++ Dwolla.Client/Models/Requests/CreateFundingSourceRequest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Dwolla.Client.Models.Requests
+{
+    public class CreatePlaidFundingSourceRequest
+    {
+        public string Name { get; set; }
+        public string PlaidToken { get; set; }
+    }
+
+    public class CreateFundingSourceRequest
+    {
+        [JsonProperty(PropertyName = "_links")]
+        public Dictionary<string, Link> Links { get; set; }
+
+        public string RoutingNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankAccountType { get; set; }
+        public string Name { get; set; }
+        public List<string> Channels { get; set; }
+    }
+}
