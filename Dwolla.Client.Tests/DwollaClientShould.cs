2b0ca93dc6c7b3a6ee0966f417656ec8061583b0
Isaiah Dahlberg <isaiah@eventlink.com>
2023-02-20 13:02:03 -0600

    Fix tests and add back UserAgent test

--- Dwolla.Client.Tests/DwollaClientShould.cs
+++ Dwolla.Client.Tests/DwollaClientShould.cs
@@ -19,6 +19,7 @@ namespace Dwolla.Client.Tests
     {
         private const string JsonV1 = "application/vnd.dwolla.v1.hal+json";
         private const string RequestId = "some-id";
+        private const string UserAgent = "dwolla-v2-csharp/5.3.0";
         private static readonly Uri RequestUri = new Uri("https://api-sandbox.dwolla.com/foo");
         private static readonly Uri AuthRequestUri = new Uri("https://accounts-sandbox.dwolla.com/foo");
         private static readonly Headers Headers = new Headers { { "key1", "value1" }, { "key2", "value2" } };
@@ -32,6 +33,13 @@ namespace Dwolla.Client.Tests
         {
             _restClient = new Mock<IRestClient>();
             _client = new DwollaClient(_restClient.Object, true);
+        }
+        
+        [Fact]
+        public void ConfigureHttpClient()
+        {
+            var client = DwollaClient.CreateHttpClient();
+            Assert.Equal(UserAgent, client.DefaultRequestHeaders.UserAgent.ToString());
         }
 
         [Fact]
@@ -176,7 +184,7 @@ namespace Dwolla.Client.Tests
         private static async void PostCallback(HttpRequestMessage expected, HttpRequestMessage actual)
         {
             GetCallback(expected, actual);
-            Assert.Equal("{\"Message\":\"requestTest\"}", await actual.Content.ReadAsStringAsync());
+            Assert.Equal("{\"message\":\"requestTest\"}", await actual.Content.ReadAsStringAsync());
             Assert.Equal("application/vnd.dwolla.v1.hal+json; charset=utf-8",
                 actual.Content.Headers.ContentType.ToString());
         }
