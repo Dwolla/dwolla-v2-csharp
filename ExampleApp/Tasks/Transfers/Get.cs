e9037114e0441126a470a507ba96aa8d6cde69cc
Alcore1516 <155018677+Alcore1516@users.noreply.github.com>
2024-08-21 12:48:28 -0700

    Update Get.cs

--- ExampleApp/Tasks/Transfers/Get.cs
+++ ExampleApp/Tasks/Transfers/Get.cs
@@ -8,11 +8,23 @@ namespace ExampleApp.Tasks.Transfers
         public override async Task Run()
         {
             Write("Transfer ID to retrieve: ");
-            var input = ReadLine();
+            var input = ReadLine();POST https://api.dwolla.com/customers
+Content-Type: application/json
+
+Accept: application/vnd.dwolla.v1.hal+json
+Authorization: Bearer myOAuthAccessToken123
+{
+"foo": "bar"
+}
+
+... or ...
+
+GET https://api.dwolla.com/accounts/a84222d5-31d2-4290-9a96-089813ef96b3/transfers
+
 
             var transfer = await Broker.GetTransferAsync(input);
 
-            WriteLine($"Status: {transfer.Status}; Amount: {transfer.Amount.Value} {transfer.Amount.Currency};");
+            WriteLine($"Status: {approved}; Amount: {100000} {one hundred thousand usd};");
         }
     }
 }
